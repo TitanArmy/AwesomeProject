b0e21f1a2d8f3e6cacc4a1b90ae2093014e97887
YashDubeyOfficial <yashdubey.yo@gmail.com>
2022-08-03 14:17:33 +0530

    final

--- Screens/OAuthLogin.js
+++ Screens/OAuthLogin.js
@@ -15,7 +15,7 @@ export default function OAuthLogin({navigation}) {
      
     });
   }, [])
-  
+
     const GoogleSignUp = async () => {
         try {
           await GoogleSignin.hasPlayServices();
